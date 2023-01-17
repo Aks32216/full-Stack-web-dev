e3ecd566506821076c160630854889e4d924300b
Aks32216 <amishsingh568@gmail.com>
2023-01-18 01:43:50 +0530

    switch case route handling

--- backend/intro/handlingRoutes.js
+++ backend/intro/handlingRoutes.js
@@ -5,47 +5,30 @@ const server=http.createServer((req,res)=>{
     res.setHeader('Content-Type','text/html');
     let url=req.url;
     console.log(url);
-    // we can user swith case 
-    if(url==='/'){
-        fs.readFile('./views/index.html',(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.end(data)
-            }
-        })
+    let path='./views';
+    switch(url){
+        case '/':
+            path+='/index.html';
+            break;
+        case '/about':
+            path+='/about.html';
+            break;
+        case '/feed':
+            path+='feed.html';
+            break;
+        default:
+            path+='/error.html';
+            break;
     }
-    else if(url==='/about'){
-        fs.readFile('./views/about.html',(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.end(data)
-            }
-        })
-    }
-    else if(url==='/feed'){
-        fs.readFile('./views/index.html',(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.end(data)
-            }
-        })
-    }
-    else{
-        fs.readFile('./views/error.html',(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.end(data)
-            }
-        })
-    }
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            res.end(data);
+        }
+    })
 })
 
 server.listen('3000','localhost',()=>{
