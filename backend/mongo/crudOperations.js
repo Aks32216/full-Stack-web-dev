86b328d2055259b57f0d2a40852dac10cc253d73
Aks32216 <amishsingh568@gmail.com>
2023-01-29 14:45:04 +0530

    bcrypt

--- backend/mongo/crudOperations.js
+++ backend/mongo/crudOperations.js
@@ -1,11 +1,7 @@
 const express=require('express');
 const app=express();
-const dotenv=require('dotenv');
-const mongoose=require('mongoose');
-const emailValidator=require('email-validator');
-dotenv.config({path:'../.env'});
-const db_link=process.env.DB_LINK;
 app.use(express.json());
+const {userModel}=require('./models/userModel');
 let users=[];
 
 const userRouter=express.Router();
@@ -74,7 +70,7 @@ async function patchUser(req,res){
 async function deleteUser(req,res){
     console.log(req.body);
     // let data=await userModel.deleteOne({email:"amish12435@gmail.com"});
-    let data=await userModel.findOneAndRemove({email:"riteshjha@gmail.com"});
+    let data=await userModel.findOneAndRemove({email:"rashmi@gmail.com"});
     console.log(data);
     res.json({
         msg:"deleted data successfully"
@@ -87,63 +83,5 @@ app.listen(5000,()=>{
 
 
 
-
-
-mongoose.set("strictQuery", false);
-mongoose.connect(db_link)
-    .then((db)=>{
-        console.log("connected to database");
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
-
-const userSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        validate:function (){
-            return emailValidator.validate(this.email);
-        }
-    },
-    password:{
-        type:String,
-        required:true,
-        minLength:7
-    },
-    confirmPassword:{
-        type:String,
-        required:true,
-        minLength:7,
-        validate: function (){
-            return this.confirmPassword==this.password;
-        }
-    }
-});
-
-// hooks are functions that run before and after the main objective function
-// userSchema.pre('save',()=>{
-//     console.log("pre save hooks");
-// })
-
-// userSchema.post('save',()=>{
-//     console.log("post save hooks");
-// })
-
-// userSchema.pre('remove',()=>{
-//     console.log("pre remove hooks");
-// })
-
-// userSchema.post('remove',()=>{
-//     console.log("post remove hooks");
-// })
-    
-// converting schema to model
-const userModel=mongoose.model('userModel',userSchema);
 
 
