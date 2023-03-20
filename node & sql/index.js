9a2a8f61d6efb653f7711a9c76fc909a5c598955
Aks32216 <amishsingh568@gmail.com>
2023-03-20 18:25:15 +0530

    inserting into database

--- node & sql/index.js
+++ node & sql/index.js
@@ -1,15 +1,29 @@
 const express=require("express");
 const connection=require("./connection");
+const path=require('path');
+const bodyParser=require('body-parser');
 
 const app=express();
 
-connection.connect((err)=>{
-    if(err)
-        throw err;
-    connection.query("select * from students",(err,result)=>{
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/register.html'));
+});
+
+app.post('/',(req,res)=>{
+    let {name,email,number}=req.body;
+    connection.connect((err)=>{
         if(err)
             throw err;
-        console.log(result);
+        let sql="INSERT INTO students(id,rollno,name,email) values(22,?,?,?)";
+        connection.query(sql,[number,name,email],(err,result)=>{
+            if(err)
+                throw err;
+            res.send("student Registered successfully"+result.insertId);
+        })
+
     })
 })
 
