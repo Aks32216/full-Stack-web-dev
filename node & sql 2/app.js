df4a2152fefce254a846c698dbef44c6f34d33dc
Aks32216 <amishsingh568@gmail.com>
2023-04-07 02:50:14 +0530

    learned file uploading with express-fileupload

--- node & sql 2/app.js
+++ node & sql 2/app.js
@@ -4,24 +4,44 @@ const path=require('path');
 const exphbs=require('express-handlebars');
 const fileUpload=require('express-fileupload');
 const port=3000;
+const mysql=require('mysql');
 
 app.engine('hbs',exphbs.engine({extname:'.hbs'}));
 app.set('view engine','hbs');
 
 app.use(fileUpload());
+app.use(express.static('public'));
+app.use(express.static('uploads'));
 
+
+const connection=mysql.createConnection({
+    host:"localhost",
+    user:"amish",
+    password:"aks32216",
+    database:"amish"
+});
+
+connection.connect((err)=>{
+    if(err)
+        throw err;
+    console.log("connected to database");
+})
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    let sql='select * from userprofile where id=1';
+    connection.query(sql,(err,result)=>{
+        if(err){
+            throw err;
+        }
+        res.render('index',{result})
+    })
 })
 
 app.post('/',(req,res)=>{
     let sampleFile;
     let uploadPath;
     
-    
     sampleFile=req.files.sampleFile;
-    console.log(sampleFile);
     uploadPath=path.join(__dirname,'/uploads',sampleFile.name);
     
     // moving to upload folder
@@ -29,7 +49,12 @@ app.post('/',(req,res)=>{
         if(err){
             return res.status(500).send("Server Error");
         }
-        res.send("File Uploaded")
+        let sql='update userprofile set profile_image=? where id=1';
+        connection.query(sql,[sampleFile.name],(err,result)=>{
+            if(err)
+                throw err;
+            res.redirect('/');
+        })
     });
 })
 
