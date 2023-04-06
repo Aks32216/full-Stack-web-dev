56f31402da3178be73ab55020640b6e0496b37ff
Aks32216 <amishsingh568@gmail.com>
2023-04-06 21:45:33 +0530

    uploaded file

--- node & sql 2/app.js
+++ node & sql 2/app.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 const exphbs=require('express-handlebars');
 const fileUpload=require('express-fileupload');
 const port=3000;
@@ -12,6 +13,24 @@ app.use(fileUpload());
 
 app.get('/',(req,res)=>{
     res.render('index')
+})
+
+app.post('/',(req,res)=>{
+    let sampleFile;
+    let uploadPath;
+    
+    
+    sampleFile=req.files.sampleFile;
+    console.log(sampleFile);
+    uploadPath=path.join(__dirname,'/uploads',sampleFile.name);
+    
+    // moving to upload folder
+    sampleFile.mv(uploadPath,(err)=>{
+        if(err){
+            return res.status(500).send("Server Error");
+        }
+        res.send("File Uploaded")
+    });
 })
 
 app.listen(port,()=>{
